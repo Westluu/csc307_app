3b64ef2d14be8565a8528097440369a5041bfc9a
Westluu <west1luu@gmail.com>
2023-04-17 09:30:56 -0700

    added Id column

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -4,73 +4,73 @@ import Form from './Form'
 import axios from 'axios';
 
 function MyApp() {
-  const [characters, setCharacters] = useState([]);
-
-  function removeOneCharacter(index) {
-      makeDeleteCall(characters[index]['id']).then( result => {
-      if (result && result.status === 204) {
-        const updated = characters.filter((character, i) => {
-        return i !== index
+    const [characters, setCharacters] = useState([]);
+    function removeOneCharacter(index) {
+        makeDeleteCall(characters[index]['id']).then(result => {
+        if (result && result.status === 204) {
+            const updated = characters.filter((character, i) => {
+            return i !== index
         });
         setCharacters(updated);
       }
       });
     }
 
-    function updateList(person) {
+  function updateList(person) {
       makePostCall(person).then( result => {
       if (result && result.status === 201)
-          setCharacters([...characters, person] );
+          console.log(result.data);
+          setCharacters([...characters, result.data]);
       });
+  }
+
+  async function fetchAll(){
+    try {
+        const response = await axios.get('http://localhost:8000/users');
+        return response.data.users_list;     
     }
-
-    async function fetchAll(){
-      try {
-         const response = await axios.get('http://localhost:8000/users');
-         return response.data.users_list;     
-      }
-      catch (error){
-         //We're not handling errors. Just logging into the console.
-         console.log(error); 
-         return false;         
-      }
+    catch (error){
+        //We're not handling errors. Just logging into the console.
+        console.log(error); 
+        return false;         
     }
+  }
 
-    async function makePostCall(person){
-      try {
-        const response = await axios.post('http://localhost:8000/users/', person);
-        return response;
-      } 
-      catch (error) {
-        console.log(error);
-        return false; 
-      }
+  async function makePostCall(person){
+    try {
+      const response = await axios.post('http://localhost:8000/users/', person);
+      return response;
+    } 
+    catch (error) {
+      console.log(error);
+      return false; 
     }
+  }
 
-    async function makeDeleteCall(id){
-      try {
-        const response = await axios.delete('http://localhost:8000/users/' + id);
-        return response;
-      } 
-      catch (error) {
-        console.log(error);
-        return false; 
-      }
+  async function makeDeleteCall(id){
+    try {
+      const response = await axios.delete('http://localhost:8000/users/' + id);
+      return response;
+    } 
+    catch (error) {
+      console.log(error);
+      return false; 
     }
+  }
 
-    useEffect(() => {
-      fetchAll().then( result => {
-        if (result)
-          setCharacters(result);
-      });
-    }, []);
+  useEffect(() => {
+    fetchAll().then( result => {
+      if (result)
+        setCharacters(result);
+    });
+  }, []);
 
-    return (
-      <div className="container">
-        <Table characterData={characters} removeCharacter={removeOneCharacter} />
-        <Form handleSubmit={updateList} />
-    </div>
-    )
+  return (
+    <div className="container">
+      <Table characterData={characters} removeCharacter={removeOneCharacter} />
+      <Form handleSubmit={updateList} />
+  </div>
+  )
 }
 
 export default MyApp;
